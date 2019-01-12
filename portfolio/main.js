e1341b7717d73f526824e313d25c7573ff06d4b8
Maddy <meb4126@rit.edu>
2019-01-11 19:34:51 -0500

    vs code updated and i think everything broke

---
+++ portfolio/main.js
@@ -0,0 +1,33 @@
+/* when it is hovered over,,, */
+$(".menuitem").hover(function(){
+    /* make it go down */
+    console.log("hamburger hover");
+    $(this).next(".submenu").slideDown();
+});
+
+/* when the mouse leaves that area, go back up. */
+$(".submenu").mouseleave(function(){
+    $(this).slideUp();
+});
+
+// // modal
+// // Get the modal
+// var modal = document.getElementById('myModal');
+
+// // Get the image and insert it inside the modal - use its "alt" text as a caption
+// var img = document.getElementById('myImg');
+// var modalImg = document.getElementById("img01");
+// var captionText = document.getElementById("caption");
+// img.onclick = function(){
+//   modal.style.display = "block";
+//   modalImg.src = this.src;
+//   captionText.innerHTML = this.p;
+// }
+
+// // Get the <span> element that closes the modal
+// var span = document.getElementsByClassName("close")[0];
+
+// // When the user clicks on <span> (x), close the modal
+// span.onclick = function() { 
+//   modal.style.display = "none";
+// }
